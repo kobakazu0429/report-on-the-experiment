81d3bea9e6070951e17c4fc3ae206df04473391b
kobakazu0429 <kobakazu10@gmail.com>
2023-01-28 21:17:39 +0900

    add

--- documents/06/勾配法と探索法の比較と離散化による誤差の測定/src/export-stdout.sh
+++ documents/06/勾配法と探索法の比較と離散化による誤差の測定/src/export-stdout.sh
@@ -1,53 +1,59 @@
-PATCH="./patch.exe"
+# PATCH="./patch.exe"
+PATCH="patch"
 CC="gcc"
 OPTIONS="gene.o -o main"
 
 OUT=out
 
 TARGETS=(
-  g1.c
+  # g1.c
   g1g.c
-  g2.c
+  # g2.c
   g2g.c
-  g3.c
+  # g3.c
   g3g.c
   ga.c
   # ga-ex.c
 )
 
 function exec_stdout() {
-  target=$1
-  out=$2
+  target="$1"
+  out="$2"
 
-  touch $out
-  echo "$ ./main" >> $out
+  touch "$out"
+  echo '$ ./main' >> "$out"
   $CC $target $OPTIONS
-  ./main >> $out
+  ./main >> "$out"
+
+  rm ./main
 
   echo "done $out"
 }
 
 function p() {
-  target=$1
-  patch_file=$2
+  target="$1"
+  patch_file="$2"
 
   out="$OUT/$patch_file.out"
 
-  echo $patch_file
+  echo "$patch_file"
 
-  $PATCH -u --binary $target < $patch_file
+  $PATCH -u --binary $target < "$patch_file"
 
-  exec_stdout $target $out
+  exec_stdout "$target" "$out"
 
-  $PATCH -R --binary $target < $patch_file
+  $PATCH -R --binary $target < "$patch_file"
 }
 
 function cleanup() {
-  rm main
   rm ./*.csv
 }
 
 function main() {
+  if [ -d $OUT ]; then
+    rm -rf $OUT
+  fi
+
   mkdir -p $OUT
 
   for target in ${TARGETS[@]}; do
